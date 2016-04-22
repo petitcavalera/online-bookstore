f9dffd673b1d1c6e29486d372423344725a29f9a
mrifkysaifulhuda <mrifkysaifulhuda@gmail.com>
2016-04-22 10:08:13 +0700

    Main Layouts

--- public/js/directives.js
+++ public/js/directives.js
@@ -40,9 +40,9 @@ function pageTitle($rootScope, $timeout) {
         link: function(scope, element) {
             var listener = function(event, toState, toParams, fromState, fromParams) {
                 // Default title - load on Dashboard 1
-                var title = 'INSPINIA | Responsive Admin Theme';
+                var title = 'The Book Store | Responsive Admin Theme';
                 // Create your own title pattern
-                if (toState.data && toState.data.pageTitle) title = 'INSPINIA | ' + toState.data.pageTitle;
+                if (toState.data && toState.data.pageTitle) title = 'The Book Store | ' + toState.data.pageTitle;
                 $timeout(function() {
                     element.text(title);
                 });
